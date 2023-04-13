3c2eef74f051b2f5659c31f57e344e2807aae304
Boudewijn Overvliet <boudewijnovervliet@BoudewiacStudio.fritz.box>
2023-04-13 21:34:02 +0200

    wip

---
+++ orval.config.js
@@ -0,0 +1,10 @@
+ module.exports = {
+    'tapp-api': {
+        output: {
+            target: 'src/propositions.ts',
+        },
+        input: {
+          target: '../../tapp/tapp-api/laravel/storage/app/docs/propositions.json',
+        },
+    },
+ };
